ce0eaa1f049b0e74b9004de67debfc6d8b9c228a
Andrija Musikic <antlr.masta@gmail.com>
2022-10-16 16:57:58 +0200

    Updated sql dumps (Zastita zivotne sredine)

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Traits\MediaUploadingTrait;
 use App\Http\Requests\MassDestroyPageRequest;
 use App\Http\Requests\StorePageRequest;
 use App\Http\Requests\UpdatePageRequest;
+use App\Models\Link;
 use App\Models\Page;
 use Gate;
 use Illuminate\Http\Request;
@@ -14,109 +15,117 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PageController extends Controller
 {
-    use MediaUploadingTrait;
+  use MediaUploadingTrait;
 
-    public function index()
-    {
-        abort_if(Gate::denies('page_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+  public function index()
+  {
+    abort_if(Gate::denies('page_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        $pages = Page::all();
+    $pages = Page::all();
 
-        return view('admin.pages.index', compact('pages'));
+    return view('admin.pages.index', compact('pages'));
+  }
+
+  public function create()
+  {
+    abort_if(Gate::denies('page_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+    return view('admin.pages.create');
+  }
+
+  public function store(StorePageRequest $request)
+  {
+    // TODO: Remove this
+    $link = Link::query()->where('title', '=', $request->title)->first();
+    $request['slug'] = $link->slug;
+
+    $page = Page::create($request->all());
+    $link->update(['page_id' => $page->id]);
+
+    $descriptions = $request->descriptions;
+
+    foreach ($request->input('attachments', []) as $key => $file) {
+      if (!array_key_exists($key, $request->descriptions)) {
+        $descriptions[$key] = '';
+      }
+      $description = $descriptions[$key];
+      $page->addMedia(storage_path('tmp/uploads/' . basename($file)))
+        ->withCustomProperties(['description' => $description])
+        ->toMediaCollection('attachments');
     }
 
-    public function create()
-    {
-        abort_if(Gate::denies('page_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+    return redirect()->route('admin.pages.index');
+  }
 
-        return view('admin.pages.create');
-    }
+  public function edit(Page $page)
+  {
+    abort_if(Gate::denies('page_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-    public function store(StorePageRequest $request)
-    {
-        $page = Page::create($request->all());
-        foreach ($request->input('attachments', []) as  $key => $file) {
-            if (!array_key_exists($key, $request->descriptions)) {
-                $descriptions[$key] = '';
-            }
-            $description = $descriptions[$key];
-            $page->addMedia(storage_path('tmp/uploads/' . basename($file)))
-                ->withCustomProperties(['description' => $description])
-                ->toMediaCollection('attachments');
+    return view('admin.pages.edit', compact('page'));
+  }
+
+  public function update(UpdatePageRequest $request, Page $page)
+  {
+    $page->update($request->all());
+
+    if (count($page->attachments) > 0) {
+      foreach ($page->attachments as $media) {
+        if (!in_array($media->file_name, $request->input('attachments', []))) {
+          $media->delete();
         }
-
-        return redirect()->route('admin.pages.index');
+      }
     }
-
-    public function edit(Page $page)
-    {
-        abort_if(Gate::denies('page_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
-        return view('admin.pages.edit', compact('page'));
-    }
-
-    public function update(UpdatePageRequest $request, Page $page)
-    {
-        $page->update($request->all());
-
-        if (count($page->attachments) > 0) {
-            foreach ($page->attachments as $media) {
-                if (!in_array($media->file_name, $request->input('attachments', []))) {
-                    $media->delete();
-                }
-            }
+    $media = $page->attachments->pluck('file_name')->toArray();
+    $descriptions = $request->descriptions;
+    foreach ($request->input('attachments', []) as $key => $file) {
+      if (count($media) === 0 || !in_array($file, $media)) {
+        if (!array_key_exists($key, $request->descriptions)) {
+          $descriptions[$key] = '';
         }
-        $media = $page->attachments->pluck('file_name')->toArray();
-        $descriptions = $request->descriptions;
-        foreach ($request->input('attachments', []) as $key => $file) {
-            if (count($media) === 0 || !in_array($file, $media)) {
-                if (!array_key_exists($key, $request->descriptions)) {
-                    $descriptions[$key] = '';
-                }
-                $description = $descriptions[$key];
-                $page->addMedia(storage_path('tmp/uploads/' . basename($file)))
-                    ->withCustomProperties(['description' => $description])
-                    ->toMediaCollection('attachments');
-            }
-        }
-
-        return redirect()->route('admin.pages.index');
+        $description = $descriptions[$key];
+        $page->addMedia(storage_path('tmp/uploads/' . basename($file)))
+          ->withCustomProperties(['description' => $description])
+          ->toMediaCollection('attachments');
+      }
     }
 
-    public function show(Page $page)
-    {
-        abort_if(Gate::denies('page_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+    return redirect()->route('admin.pages.index');
+  }
 
-        $page->load('pagePublicPurchases', 'pagePublicCompetitions', 'pagePosts', 'pageLinks');
+  public function show(Page $page)
+  {
+    abort_if(Gate::denies('page_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        return view('admin.pages.show', compact('page'));
-    }
+    $page->load('pagePublicPurchases', 'pagePublicCompetitions', 'pagePosts', 'pageLinks');
 
-    public function destroy(Page $page)
-    {
-        abort_if(Gate::denies('page_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+    return view('admin.pages.show', compact('page'));
+  }
 
-        $page->delete();
+  public function destroy(Page $page)
+  {
+    abort_if(Gate::denies('page_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        return back();
-    }
+    $page->delete();
 
-    public function massDestroy(MassDestroyPageRequest $request)
-    {
-        Page::whereIn('id', request('ids'))->delete();
+    return back();
+  }
 
-        return response(null, Response::HTTP_NO_CONTENT);
-    }
+  public function massDestroy(MassDestroyPageRequest $request)
+  {
+    Page::whereIn('id', request('ids'))->delete();
 
-    public function storeCKEditorImages(Request $request)
-    {
-        abort_if(Gate::denies('post_create') && Gate::denies('post_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+    return response(null, Response::HTTP_NO_CONTENT);
+  }
 
-        $model = new Page();
-        $model->id = $request->input('crud_id', 0);
-        $model->exists = true;
-        $media = $model->addMediaFromRequest('upload')->toMediaCollection('ck-media');
+  public function storeCKEditorImages(Request $request)
+  {
+    abort_if(Gate::denies('post_create') && Gate::denies('post_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        return response()->json(['id' => $media->id, 'url' => $media->getUrl()], Response::HTTP_CREATED);
-    }
+    $model = new Page();
+    $model->id = $request->input('crud_id', 0);
+    $model->exists = true;
+    $media = $model->addMediaFromRequest('upload')->toMediaCollection('ck-media');
+
+    return response()->json(['id' => $media->id, 'url' => $media->getUrl()], Response::HTTP_CREATED);
+  }
 }
