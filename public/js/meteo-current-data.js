8a8616d2017b451f9476bc8fff96cae02f6d80d8
Marko Pejanovic <mare.sampbn@gmail.com>
2023-02-28 11:04:47 +0100

    - page/meteorologija-aktuelni-podaci: Prebaciti u jedan red

--- public/js/meteo-current-data.js
+++ public/js/meteo-current-data.js
@@ -8,8 +8,14 @@ document.addEventListener('DOMContentLoaded', function () {
         responsive: true,
         "columns": [
             { "data": "station_name" },
-            { "data": "description" },
-            { "data": "image" }
+            { "data": "period" },
+            { "data": "temperature" },
+            { "data": "pressure" },
+            { "data": "wind" },
+            { "data": "rainfall" },
+            { "data": "min_temp" },
+            { "data": "max_temp" },
+            { "data": "snow" }
         ],
         "language": {
             "url": "../js/Datatable/Serbian.json"
